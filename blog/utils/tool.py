de2d26e464e0058051c3a7b53527d897efcaadc5
wangjiancn <wangjianchn@outlook.com>
2019-06-17 00:13:36 +0800

    feat:增加URL查询参数解析方法
    将查询参数解析为分页参数/排序参数/筛选参数

--- blog/utils/tool.py
+++ blog/utils/tool.py
@@ -1,4 +1,7 @@
 import uuid
+from collections import namedtuple
+from django.http import QueryDict
+from typing import Tuple, NamedTuple
 
 
 def is_uuid(arg):
@@ -9,3 +12,29 @@ def is_uuid(arg):
         return False
     else:
         return True
+
+
+def parse_query_string(queryDict: QueryDict) -> NamedTuple('query', pagination=dict, order_by=list, filters=dict):
+    """从请求中解析查询参数
+
+    Args:
+        queryDict ([django.http.QueryDict]): 接受request.GET作为参数,
+             官方链接:https://docs.djangoproject.com/en/2.2/ref/request-response/#django.http.QueryDict
+
+    Returns:
+        NamedTuple: NamedTuple('query', pagination=dict, order_by=list, filters=dict)
+    """
+
+    _clone = queryDict.copy()
+
+    Query = namedtuple('Query', 'pagination, order_by, filters')
+
+    order_by = _clone.pop('order_by', [])
+    pagination = {
+        'limit': _clone.pop('limit', [10])[-1],
+        'offset': _clone.pop('offset', [0])[-1],
+    }
+    filters = _clone.dict()
+
+    query = Query(pagination, order_by, filters)
+    return query
